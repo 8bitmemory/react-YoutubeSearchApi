ec7b063b4c636d8999cedc5c702b8ebadd77029a
Elliott Esponda <e_e102@txstate.edu>
2019-07-17 01:39:04 -0500

    adds app class componenet and Youtube api key

--- src/index.js
+++ src/index.js
@@ -1,15 +1,49 @@
+import _ from 'lodash';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+import YouTubeSearch from 'youtube-api-search';
+import SearchBar from './components/search_bar'
+import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
 
-import App from './components/app';
-import reducers from './reducers';
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+const API_KEY = 'AIzaSyBqc4kabmMni1F0c4sFd6_GX1B7Cc-gsMQ';
 
-ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+class App extends React.Component {
+    constructor (props){
+        super(props);
+
+        this.state = { 
+            videos: [],
+            selectedVideo: null
+        };
+        
+        this.videoSearch('Surfboards');
+    }
+
+    videoSearch(term) {
+        YouTubeSearch({key: API_KEY, term: term}, (videos) => {
+            this.setState({ 
+                videos: videos,
+                selectedVideo: videos[0]
+            });
+        });
+    }
+
+    render() {
+        const videoSearch = _.debounce((term) => { this.videoSearch(term) },400);
+        
+        return (
+            <div>
+                <SearchBar onSearchTermChange={videoSearch} />
+                <VideoDetail video={this.state.selectedVideo} />
+                <VideoList 
+                    onVideoSelect={selectedVideo => this.setState({selectedVideo})}
+                    videos={this.state.videos} />
+            </div>
+        );
+    }
+    
+}
+
+ReactDOM.render(<App></App>, document.querySelector('.container'))
